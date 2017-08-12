9c443b53b109e6dcc0be88eddc2f445c8fea6007
Andrew Freiday <andrewfreiday@gmail.com>
2017-08-12 14:58:27 -0400

    Add event to get state from window, setup to turn signal and brake light displays

--- client/src/app/components/wrx-overhead/wrx-overhead.component.ts
+++ client/src/app/components/wrx-overhead/wrx-overhead.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: '[wrx-overhead]',
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   styles: [ require('./wrx-overhead.component.scss') ]
 })
 export class WrxOverheadComponent {
+  public state: any;
+
   constructor() { }
+
+  public headlights_on() {
+    return true;
+  }
+
+  public brakes_on() {
+    return parseInt(this.state.brake) > 0;
+  }
+
+  public turn_signal_left() {
+    return this.state.turn_signal === 'L';
+  }
+
+  public turn_signal_right() {
+    return this.state.turn_signal === 'R';
+  }
+
+  @HostListener('window:state_update', ['$event'])
+  stateListener(event: any) {
+    this.state = (window as any).state;
+  }
 }
 
